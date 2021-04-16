c871645f82aad2caf0a88b2634c23fb717091a5c
Louis Moreau <luisomoreau@Louiss-MacBook-Pro.local>
2021-04-16 16:39:52 +0200

    Added two examples - Basic & Advanced image classification

---
+++ Basic-Image-Classification/Basic-Image-Classification.ino
@@ -0,0 +1,347 @@
+/* Includes ---------------------------------------------------------------- */
+#include <Arduino.h>
+#include <WiFi.h>
+
+// Modify the following line according to your project name
+// Do not forget to import the library using "Sketch">"Include Library">"Add .ZIP Library..."
+#include <esp32-cam_image-classification_inference.h>
+
+#include "esp_http_server.h"
+#include "esp_timer.h"
+
+#include "img_converters.h"
+
+#include "esp_camera.h"
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+
+#include "camera_pins.h"
+
+// raw frame buffer from the camera
+#define FRAME_BUFFER_COLS           240
+#define FRAME_BUFFER_ROWS           240
+
+#define CUTOUT_COLS                 EI_CLASSIFIER_INPUT_WIDTH
+#define CUTOUT_ROWS                 EI_CLASSIFIER_INPUT_HEIGHT
+const int cutout_row_start = (FRAME_BUFFER_ROWS - CUTOUT_ROWS) / 2;
+const int cutout_col_start = (FRAME_BUFFER_COLS - CUTOUT_COLS) / 2;
+
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+
+
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+httpd_handle_t camera_httpd = NULL;
+httpd_handle_t stream_httpd = NULL;
+
+
+const char* ssid = "your-ssid";
+const char* password = "your-password";
+
+camera_fb_t * fb = NULL;
+uint8_t * _jpg_buf = NULL;
+
+void r565_to_rgb(uint16_t color, uint8_t *r, uint8_t *g, uint8_t *b) {
+    *r = (color & 0xF800) >> 8;
+    *g = (color & 0x07E0) >> 3;
+    *b = (color & 0x1F) << 3;
+}
+
+int cutout_get_data(size_t offset, size_t length, float *out_ptr) {
+    // so offset and length naturally operate on the *cutout*, so we need to cut it out from the real framebuffer
+    size_t bytes_left = length;
+    size_t out_ptr_ix = 0;
+    
+    // read byte for byte
+    while (bytes_left != 0) {
+        // find location of the byte in the cutout
+        size_t cutout_row = floor(offset / CUTOUT_COLS);
+        size_t cutout_col = offset - (cutout_row * CUTOUT_COLS);
+
+        // then read the value from the real frame buffer
+        size_t frame_buffer_row = cutout_row + cutout_row_start;
+        size_t frame_buffer_col = cutout_col + cutout_col_start;
+        
+        uint16_t pixelTemp = fb->buf[(frame_buffer_row * FRAME_BUFFER_COLS) + frame_buffer_col];
+
+        uint16_t pixel = (pixelTemp>>8) | (pixelTemp<<8);
+
+        uint8_t r, g, b;
+        r565_to_rgb(pixel, &r, &g, &b);
+        float pixel_f = (r << 16) + (g << 8) + b;
+        out_ptr[out_ptr_ix] = pixel_f;
+  
+        out_ptr_ix++;
+        offset++;
+        bytes_left--;
+    }
+
+    // and done!
+    return 0;
+}
+
+void classify() {
+    ei_printf("Edge Impulse standalone inferencing (Arduino)\n");
+   
+    ei_impulse_result_t result = { 0 };
+
+    // Convert to RGB888
+    //fmt2rgb888(fb->buf, fb->len, PIXFORMAT_RGB888, _jpg_buf);
+
+
+    //Serial.println("Signal...");
+    // Set up pointer to look after data, crop it and convert it to RGB888
+    signal_t signal;
+    signal.total_length = CUTOUT_COLS * CUTOUT_ROWS;
+    signal.get_data = &cutout_get_data;
+  
+    // Feed signal to the classifier
+    EI_IMPULSE_ERROR res = run_classifier(&signal, &result, false /* debug */);
+
+    // Returned error variable "res" while data object.array in "result" 
+    ei_printf("run_classifier returned: %d\n", res);
+    if (res != 0) return;
+
+    // print the predictions
+    ei_printf("Predictions ");
+    ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
+        result.timing.dsp, result.timing.classification, result.timing.anomaly);
+    ei_printf(": \n");
+
+    // Print short form result data
+    ei_printf("[");
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+        ei_printf("%.5f", result.classification[ix].value);
+        #if EI_CLASSIFIER_HAS_ANOMALY == 1
+             ei_printf(", ");
+        #else
+           if (ix != EI_CLASSIFIER_LABEL_COUNT - 1) {
+               ei_printf(", ");
+           }
+        #endif
+    }
+    
+    #if EI_CLASSIFIER_HAS_ANOMALY == 1
+         ei_printf("%.3f", result.anomaly);
+    #endif
+    ei_printf("]\n");
+
+    // human-readable predictions
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+        ei_printf("    %s: %.5f\n", result.classification[ix].label, result.classification[ix].value);
+    }
+    #if EI_CLASSIFIER_HAS_ANOMALY == 1
+        ei_printf("    anomaly score: %.3f\n", result.anomaly);
+    #endif
+
+   
+}
+
+static esp_err_t capture_handler(httpd_req_t *req){
+    Serial.println("Capture image");
+    
+    esp_err_t res = ESP_OK;
+    fb = esp_camera_fb_get();
+    if (!fb) {
+        Serial.println("Camera capture failed");
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+
+    classify();
+
+    httpd_resp_set_type(req, "image/jpeg");
+    httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+ 
+    res = httpd_resp_send(req, (const char *)fb->buf, fb->len);
+    esp_camera_fb_return(fb);
+
+    return res;
+}
+
+static esp_err_t page_handler(httpd_req_t *req) {
+
+    httpd_resp_set_type(req, "text/html");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+   // httpd_resp_send(req, page, sizeof(page));
+}
+
+static esp_err_t stream_handler(httpd_req_t *req){
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len = 0;
+    uint8_t * _jpg_buf = NULL;
+    char * part_buf[64];
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if(res != ESP_OK){
+        return res;
+    }
+
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+
+    while(true){
+        fb = esp_camera_fb_get();
+        if (!fb) {
+            Serial.println("Camera capture failed");
+            res = ESP_FAIL;
+        } else {
+               if(fb->format != PIXFORMAT_JPEG){
+                    bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+                    esp_camera_fb_return(fb);
+                    fb = NULL;
+                    if(!jpeg_converted){
+                        Serial.println("JPEG compression failed");
+                        res = ESP_FAIL;
+                    }
+                } else {
+                    _jpg_buf_len = fb->len;
+                    _jpg_buf = fb->buf;
+                }
+             }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if(res == ESP_OK){
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if(fb){
+            esp_camera_fb_return(fb);
+            fb = NULL;
+            _jpg_buf = NULL;
+        } else if(_jpg_buf){
+            free(_jpg_buf);
+            _jpg_buf = NULL;
+        }
+        if(res != ESP_OK){
+            break;
+        }
+    }
+    return res;
+}
+
+void startCameraServer(){
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    httpd_uri_t index_uri = {
+        .uri       = "/",
+        .method    = HTTP_GET,
+        .handler   = stream_handler,
+        .user_ctx  = NULL
+    };
+
+    httpd_uri_t capture_uri = {
+        .uri       = "/",
+        .method    = HTTP_GET,
+        .handler   = capture_handler,
+        .user_ctx  = NULL
+    };
+
+
+    Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+    if (httpd_start(&camera_httpd, &config) == ESP_OK) {
+        httpd_register_uri_handler(camera_httpd, &capture_uri);
+        //httpd_register_uri_handler(camera_httpd, &page_uri);
+    }
+
+    // start stream using another webserver
+    config.server_port += 1;
+    config.ctrl_port += 1;
+    Serial.printf("Starting stream server on port: '%d'\n", config.server_port);
+    if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+        httpd_register_uri_handler(stream_httpd, &index_uri);
+    }
+
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_240X240;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, 0); // lower the saturation
+  }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(10);
+}
--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# example-esp32-cam
-Builds and runs an exported image classification impulse on ESP32 Cam
+# ESP32 Cam and Edge Impulse
+
+How to run custom inference on a ESP32 cam using Edge Impulse. 
+
+### Material
+
+![esp32-cam](Documentation/esp32-cam.jpg)
+
+This code has been tested the AI Thinker ESP32 Cam module. It should work the same with the Wrover board or an board that has PSRAM. 
+
+To use this board, please select your board in the Arduino code the following lines:
+
+```
+// Select camera model
+
+#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+```
+
+## Steps
+
+* Create your Image Classification model using [Edge Impulse](https://edgeimpulse.com)
+* Download the Arduino library under the `Deployment` tab in the Edge Impulse studio
+![dl-arduino-lib](Documentation/deployment-tab.png)
+
+### Basic Image Classification Example
+
+*Note: This project does not do a proper resize of the image capture but cutout the data*
+
+* Open the `Basic-Image-Classification.ino` file under the `/Basic-Image-Classification` folder.
+* Import the .zip library you have downloaded from Edge Impulse Studio
+![import-zip](Documentation/import-zip.png)
+* Change the `#include <esp32-cam_image-classification_inference.h>` line according to your project name.
+* Compile and deploy the code to your board
+* Open the serial monitor and use the provided IP to capture an image and run the inference:
+![serial-monitor](Documentation/open-serial.png)
+![inference](Documentation/inference.png)
+
+### Advanced Image Classification Example
+
+*Note: Here we use the ESP SDK to resize the image in RGB888 format using the bilinear interpolation technique. You can see the funtion declaration [on Espressif's Github repository](https://github.com/espressif/arduino-esp32/blob/master/tools/sdk/esp32/include/esp-face/image_util/include/image_util.h#L335).
+
+* Open the `Basic-Image-Classification.ino` file under the `/Advanced-Image-Classification` folder.
+* Set your WIFI credentials
+* Navigate to the `app_httpd.cpp` tab.
+* Import the .zip library you have downloaded from Edge Impulse Studio like on the previous example:
+![import-zip](Documentation/import-zip.png)
+* Change the `#include <euros_coins_classification_inference.h>` line according to your project name.
+* Compile and deploy the code to your board
+* Open the serial monitor and use the provided IP to capture an image and run the inference:
+![serial-monitor](Documentation/open-serial-2.png)
+* On your brower navigate to the IP provided by your Serial Console
+* Use the toggle button to activate the `Edge Impulse Inference` (you need to select an image resolution lower or equal to QVGA).
+* Click on `Run inference`:
+![inference-50c](Documentation/inference-50c.png)
+![inference-1e](Documentation/inference-1e.png)
+
+## Ressources
+
+*Note: Theses tutorials / repositories have been used to create this project:*
+
+- [TinyML ESP32-CAM: Edge Image classification with Edge Impulse](https://www.survivingwithandroid.com/tinyml-esp32-cam-edge-image-classification-with-edge-impulse/) 
+- [https://github.com/v12345vtm/esp32-cam-webserver-arduino-simplified-arduino-html](https://github.com/v12345vtm/esp32-cam-webserver-arduino-simplified-arduino-html)
