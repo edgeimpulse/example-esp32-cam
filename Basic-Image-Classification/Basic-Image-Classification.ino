8885ca9b61e0e1bb3e5ebe2854076c008b426fac
luisomoreau <luis.omoreau@gmail.com>
2021-09-09 20:05:05 +0200

    Changed raw_feature_get_data to fix same prediction issue + added inference on boot sketch

--- Basic-Image-Classification/Basic-Image-Classification.ino
+++ Basic-Image-Classification/Basic-Image-Classification.ino
@@ -2,15 +2,14 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-// Modify the following line according to your project name
-// Do not forget to import the library using "Sketch">"Include Library">"Add .ZIP Library..."
-#include <esp32-cam_image-classification_inference.h>
+/* Modify the following line according to your project name
+   Do not forget to import the library using "Sketch">"Include Library">"Add .ZIP Library..."
+*/
+#include <Car_Detection_inferencing.h>
 
 #include "esp_http_server.h"
-#include "esp_timer.h"
-
 #include "img_converters.h"
-
+#include "image_util.h"
 #include "esp_camera.h"
 
 // Select camera model
@@ -24,16 +23,6 @@
 //#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
 
 #include "camera_pins.h"
-
-// raw frame buffer from the camera
-#define FRAME_BUFFER_COLS           240
-#define FRAME_BUFFER_ROWS           240
-
-#define CUTOUT_COLS                 EI_CLASSIFIER_INPUT_WIDTH
-#define CUTOUT_ROWS                 EI_CLASSIFIER_INPUT_HEIGHT
-const int cutout_row_start = (FRAME_BUFFER_ROWS - CUTOUT_ROWS) / 2;
-const int cutout_col_start = (FRAME_BUFFER_COLS - CUTOUT_COLS) / 2;
-
 
 #define PART_BOUNDARY "123456789000000000000987654321"
 
@@ -49,229 +38,12 @@ httpd_handle_t stream_httpd = NULL;
 const char* ssid = "your-ssid";
 const char* password = "your-password";
 
-camera_fb_t * fb = NULL;
-uint8_t * _jpg_buf = NULL;
-
-void r565_to_rgb(uint16_t color, uint8_t *r, uint8_t *g, uint8_t *b) {
-    *r = (color & 0xF800) >> 8;
-    *g = (color & 0x07E0) >> 3;
-    *b = (color & 0x1F) << 3;
-}
-
-int cutout_get_data(size_t offset, size_t length, float *out_ptr) {
-    // so offset and length naturally operate on the *cutout*, so we need to cut it out from the real framebuffer
-    size_t bytes_left = length;
-    size_t out_ptr_ix = 0;
-    
-    // read byte for byte
-    while (bytes_left != 0) {
-        // find location of the byte in the cutout
-        size_t cutout_row = floor(offset / CUTOUT_COLS);
-        size_t cutout_col = offset - (cutout_row * CUTOUT_COLS);
-
-        // then read the value from the real frame buffer
-        size_t frame_buffer_row = cutout_row + cutout_row_start;
-        size_t frame_buffer_col = cutout_col + cutout_col_start;
-        
-        uint16_t pixelTemp = fb->buf[(frame_buffer_row * FRAME_BUFFER_COLS) + frame_buffer_col];
-
-        uint16_t pixel = (pixelTemp>>8) | (pixelTemp<<8);
-
-        uint8_t r, g, b;
-        r565_to_rgb(pixel, &r, &g, &b);
-        float pixel_f = (r << 16) + (g << 8) + b;
-        out_ptr[out_ptr_ix] = pixel_f;
-  
-        out_ptr_ix++;
-        offset++;
-        bytes_left--;
-    }
-
-    // and done!
-    return 0;
-}
-
-void classify() {
-    ei_printf("Edge Impulse standalone inferencing (Arduino)\n");
-   
-    ei_impulse_result_t result = { 0 };
-
-    // Convert to RGB888
-    //fmt2rgb888(fb->buf, fb->len, PIXFORMAT_RGB888, _jpg_buf);
-
-
-    //Serial.println("Signal...");
-    // Set up pointer to look after data, crop it and convert it to RGB888
-    signal_t signal;
-    signal.total_length = CUTOUT_COLS * CUTOUT_ROWS;
-    signal.get_data = &cutout_get_data;
-  
-    // Feed signal to the classifier
-    EI_IMPULSE_ERROR res = run_classifier(&signal, &result, false /* debug */);
-
-    // Returned error variable "res" while data object.array in "result" 
-    ei_printf("run_classifier returned: %d\n", res);
-    if (res != 0) return;
-
-    // print the predictions
-    ei_printf("Predictions ");
-    ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
-        result.timing.dsp, result.timing.classification, result.timing.anomaly);
-    ei_printf(": \n");
-
-    // Print short form result data
-    ei_printf("[");
-    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
-        ei_printf("%.5f", result.classification[ix].value);
-        #if EI_CLASSIFIER_HAS_ANOMALY == 1
-             ei_printf(", ");
-        #else
-           if (ix != EI_CLASSIFIER_LABEL_COUNT - 1) {
-               ei_printf(", ");
-           }
-        #endif
-    }
-    
-    #if EI_CLASSIFIER_HAS_ANOMALY == 1
-         ei_printf("%.3f", result.anomaly);
-    #endif
-    ei_printf("]\n");
-
-    // human-readable predictions
-    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
-        ei_printf("    %s: %.5f\n", result.classification[ix].label, result.classification[ix].value);
-    }
-    #if EI_CLASSIFIER_HAS_ANOMALY == 1
-        ei_printf("    anomaly score: %.3f\n", result.anomaly);
-    #endif
-
-   
-}
-
-static esp_err_t capture_handler(httpd_req_t *req){
-    Serial.println("Capture image");
-    
-    esp_err_t res = ESP_OK;
-    fb = esp_camera_fb_get();
-    if (!fb) {
-        Serial.println("Camera capture failed");
-        httpd_resp_send_500(req);
-        return ESP_FAIL;
-    }
-
-    classify();
-
-    httpd_resp_set_type(req, "image/jpeg");
-    httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
-    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
- 
-    res = httpd_resp_send(req, (const char *)fb->buf, fb->len);
-    esp_camera_fb_return(fb);
-
-    return res;
-}
-
-static esp_err_t page_handler(httpd_req_t *req) {
-
-    httpd_resp_set_type(req, "text/html");
-    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
-   // httpd_resp_send(req, page, sizeof(page));
-}
-
-static esp_err_t stream_handler(httpd_req_t *req){
-    camera_fb_t * fb = NULL;
-    esp_err_t res = ESP_OK;
-    size_t _jpg_buf_len = 0;
-    uint8_t * _jpg_buf = NULL;
-    char * part_buf[64];
-
-    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-    if(res != ESP_OK){
-        return res;
-    }
-
-    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
-
-    while(true){
-        fb = esp_camera_fb_get();
-        if (!fb) {
-            Serial.println("Camera capture failed");
-            res = ESP_FAIL;
-        } else {
-               if(fb->format != PIXFORMAT_JPEG){
-                    bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-                    esp_camera_fb_return(fb);
-                    fb = NULL;
-                    if(!jpeg_converted){
-                        Serial.println("JPEG compression failed");
-                        res = ESP_FAIL;
-                    }
-                } else {
-                    _jpg_buf_len = fb->len;
-                    _jpg_buf = fb->buf;
-                }
-             }
-        if(res == ESP_OK){
-            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-        }
-        if(res == ESP_OK){
-            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
-            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
-        }
-        if(res == ESP_OK){
-            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-        }
-        if(fb){
-            esp_camera_fb_return(fb);
-            fb = NULL;
-            _jpg_buf = NULL;
-        } else if(_jpg_buf){
-            free(_jpg_buf);
-            _jpg_buf = NULL;
-        }
-        if(res != ESP_OK){
-            break;
-        }
-    }
-    return res;
-}
-
-void startCameraServer(){
-    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-
-    httpd_uri_t index_uri = {
-        .uri       = "/",
-        .method    = HTTP_GET,
-        .handler   = stream_handler,
-        .user_ctx  = NULL
-    };
-
-    httpd_uri_t capture_uri = {
-        .uri       = "/",
-        .method    = HTTP_GET,
-        .handler   = capture_handler,
-        .user_ctx  = NULL
-    };
-
-
-    Serial.printf("Starting web server on port: '%d'\n", config.server_port);
-    if (httpd_start(&camera_httpd, &config) == ESP_OK) {
-        httpd_register_uri_handler(camera_httpd, &capture_uri);
-        //httpd_register_uri_handler(camera_httpd, &page_uri);
-    }
-
-    // start stream using another webserver
-    config.server_port += 1;
-    config.ctrl_port += 1;
-    Serial.printf("Starting stream server on port: '%d'\n", config.server_port);
-    if (httpd_start(&stream_httpd, &config) == ESP_OK) {
-        httpd_register_uri_handler(stream_httpd, &index_uri);
-    }
-
-}
+dl_matrix3du_t *resized_matrix = NULL;
+size_t out_len = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+ei_impulse_result_t result = {0};
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
 
@@ -339,6 +111,142 @@ void setup() {
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
+}
+
+int raw_feature_get_data(size_t offset, size_t out_len, float *signal_ptr)
+{
+  size_t pixel_ix = offset * 3;
+  size_t bytes_left = out_len;
+  size_t out_ptr_ix = 0;
+
+  // read byte for byte
+  while (bytes_left != 0) {
+    // grab the values and convert to r/g/b
+    uint8_t r, g, b;
+    r = resized_matrix->item[pixel_ix];
+    g = resized_matrix->item[pixel_ix + 1];
+    b = resized_matrix->item[pixel_ix + 2];
+
+    // then convert to out_ptr format
+    float pixel_f = (r << 16) + (g << 8) + b;
+    signal_ptr[out_ptr_ix] = pixel_f;
+
+    // and go to the next pixel
+    out_ptr_ix++;
+    pixel_ix += 3;
+    bytes_left--;
+  }
+  return 0;
+}
+
+void classify()
+{
+  Serial.println("Getting signal...");
+  signal_t signal;
+  signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_WIDTH;
+  signal.get_data = &raw_feature_get_data;
+
+  Serial.println("Run classifier...");
+  // Feed signal to the classifier
+  EI_IMPULSE_ERROR res = run_classifier(&signal, &result, false /* debug */);
+
+  // Returned error variable "res" while data object.array in "result"
+  ei_printf("run_classifier returned: %d\n", res);
+  if (res != 0)
+    return;
+
+  // print the predictions
+  ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+            result.timing.dsp, result.timing.classification, result.timing.anomaly);
+  for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+    ei_printf("    %s: \t%f\r\n", result.classification[ix].label, result.classification[ix].value);
+  }
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+  ei_printf("    anomaly score: %f\r\n", result.anomaly);
+#endif
+}
+
+static esp_err_t capture_handler(httpd_req_t *req) {
+
+  esp_err_t res = ESP_OK;
+  camera_fb_t * fb = NULL;
+
+  Serial.println("Capture image");
+  fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    httpd_resp_send_500(req);
+    return ESP_FAIL;
+  }
+
+  // --- Convert frame to RGB888  ---
+
+  Serial.println("Converting to RGB888...");
+  // Allocate rgb888_matrix buffer
+  dl_matrix3du_t *rgb888_matrix = dl_matrix3du_alloc(1, fb->width, fb->height, 3);
+  fmt2rgb888(fb->buf, fb->len, fb->format, rgb888_matrix->item);
+
+  // --- Resize the RGB888 frame to 96x96 in this example ---
+
+  Serial.println("Resizing the frame buffer...");
+  resized_matrix = dl_matrix3du_alloc(1, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT, 3);
+  image_resize_linear(resized_matrix->item, rgb888_matrix->item, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT, 3, fb->width, fb->height);
+
+  // --- Free memory ---
+
+  dl_matrix3du_free(rgb888_matrix);
+  esp_camera_fb_return(fb);
+
+  classify();
+
+  // --- Convert back the resized RGB888 frame to JPG to send it back to the web app ---
+
+  Serial.println("Converting resized RGB888 frame to JPG...");
+  uint8_t * _jpg_buf = NULL;
+  fmt2jpg(resized_matrix->item, out_len, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT, PIXFORMAT_RGB888, 10, &_jpg_buf, &out_len);
+
+  // --- Send response ---
+
+  Serial.println("Sending back HTTP response...");
+  httpd_resp_set_type(req, "image/jpeg");
+  httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
+  httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+
+  res = httpd_resp_send(req, (const char *)_jpg_buf, out_len);
+  
+  // --- Free memory ---
+  
+  dl_matrix3du_free(resized_matrix);
+  free(_jpg_buf);
+  _jpg_buf = NULL;
+
+  return res;
+}
+
+static esp_err_t page_handler(httpd_req_t *req) {
+
+  httpd_resp_set_type(req, "text/html");
+  httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+  // httpd_resp_send(req, page, sizeof(page));
+}
+
+void startCameraServer() {
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+  httpd_uri_t capture_uri = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = capture_handler,
+    .user_ctx  = NULL
+  };
+
+
+  Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+  if (httpd_start(&camera_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(camera_httpd, &capture_uri);
+    //httpd_register_uri_handler(camera_httpd, &page_uri);
+  }
+
 }
 
 void loop() {
---
+++ inference_on_startup/inference_on_startup.ino
@@ -0,0 +1,282 @@
+#include "esp_camera.h"
+#include "esp_timer.h"
+#include "Arduino.h"
+#include "img_converters.h"
+#include "image_util.h"
+#include "FS.h"                // SD Card ESP32
+#include "SD_MMC.h"            // SD Card ESP32
+#include "soc/soc.h"           // Disable brownour problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownour problems
+#include "driver/rtc_io.h"
+#include <EEPROM.h>            // read and write from flash memory
+
+/* Modify the following line according to your project name
+   Do not forget to import the library using "Sketch">"Include Library">"Add .ZIP Library..."
+*/
+#include <Car_Detection_inferencing.h>
+
+// define the number of bytes you want to access
+#define EEPROM_SIZE 1
+
+// Pin definition for CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+int pictureNumber = 0;
+
+dl_matrix3du_t *resized_matrix = NULL;
+size_t out_len = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+ei_impulse_result_t result = {0};
+bool sd_card = true;
+
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+
+  Serial.begin(115200);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_QVGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Init Camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  //Serial.println("Starting SD Card");
+  if (!SD_MMC.begin()) {
+    Serial.println("SD Card Mount Failed");
+    sd_card = false;
+  }
+
+  uint8_t cardType = SD_MMC.cardType();
+  if (cardType == CARD_NONE) {
+    Serial.println("No SD Card attached");
+    sd_card = false;
+  }
+
+}
+
+void take_picture()
+{
+  camera_fb_t * fb = NULL;
+  bool s;
+
+  // --- Taking picture ---
+
+  int64_t time_start = esp_timer_get_time();
+  fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+  int64_t time_end = esp_timer_get_time();
+  Serial.printf("JPG: %uB %ums\n", (uint32_t)(fb->len), (uint32_t)((time_end - time_start) / 1000));
+
+  // --- Save Orginal Picture ---
+
+  if (sd_card) {
+    // initialize EEPROM with predefined size
+    EEPROM.begin(EEPROM_SIZE);
+    pictureNumber = EEPROM.read(0) + 1;
+
+    // Path where new picture will be saved in SD Card
+    String path = "/" + String(pictureNumber) + "_original" + ".jpg";
+
+    fs::FS &fs = SD_MMC;
+    Serial.printf("Picture file name: %s\n", path.c_str());
+
+    File file = fs.open(path.c_str(), FILE_WRITE);
+    if (!file) {
+      Serial.println("Failed to open file in writing mode");
+    }
+    else {
+      file.write(fb->buf, fb->len); // payload (image), payload length
+      Serial.printf("Saved file to path: %s\n", path.c_str());
+      EEPROM.write(0, pictureNumber);
+      EEPROM.commit();
+    }
+    file.close();
+  }
+
+  // --- Convert frame to RGB888  ---
+
+  Serial.println("Converting to RGB888...");
+  time_start = esp_timer_get_time();
+  // Allocate rgb888_matrix buffer
+  dl_matrix3du_t *rgb888_matrix = dl_matrix3du_alloc(1, fb->width, fb->height, 3);
+  s = fmt2rgb888(fb->buf, fb->len, fb->format, rgb888_matrix->item);
+  time_end = esp_timer_get_time();
+  Serial.printf("Done in %ums\n", (uint32_t)((time_end - time_start) / 1000));
+
+  // --- Resize the RGB888 frame to 96x96 in this example ---
+
+  Serial.println("Resizing the frame buffer...");
+  time_start = esp_timer_get_time();
+  resized_matrix = dl_matrix3du_alloc(1, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT, 3);
+  image_resize_linear(resized_matrix->item, rgb888_matrix->item, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT, 3, fb->width, fb->height);
+  time_end = esp_timer_get_time();
+  Serial.printf("Done in %ums\n", (uint32_t)((time_end - time_start) / 1000));
+
+  // --- Free memory ---
+
+  dl_matrix3du_free(rgb888_matrix);
+  esp_camera_fb_return(fb);
+
+  // --- Call classifier function ---
+
+  classify();
+
+  // --- Convert back the resized RGB888 frame to JPG to save on SD card ---
+
+  Serial.println("Converting ei_buf to JPG...");
+  time_start = esp_timer_get_time();
+  // Allocate out_matrix buffer
+  dl_matrix3du_t *jpg_out_matrix = dl_matrix3du_alloc(1, fb->width, fb->height, 3);
+  s = fmt2jpg(resized_matrix->item, out_len, EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT, PIXFORMAT_RGB888, 10, &jpg_out_matrix->item, &out_len);
+  time_end = esp_timer_get_time();
+  Serial.printf("Done in %ums\n", (uint32_t)((time_end - time_start) / 1000));
+
+  // --- Free memory ---
+  dl_matrix3du_free(resized_matrix);
+
+
+  //--- Save resized Picture ---
+  if (sd_card) {
+    // Path where new picture will be saved in SD Card
+    String path = "/" + String(pictureNumber) + "_resized" + ".jpg";
+
+    fs::FS &fs = SD_MMC;
+    Serial.printf("Picture file name: %s\n", path.c_str());
+
+    File file = fs.open(path.c_str(), FILE_WRITE);
+    if (!file) {
+      Serial.println("Failed to open file in writing mode");
+    }
+    else {
+      file.write(jpg_out_matrix->item, out_len); // payload (image), payload length
+      Serial.printf("Saved file to path: %s\n", path.c_str());
+    }
+    file.close();
+  }
+
+  // --- Free memory ---
+  dl_matrix3du_free(jpg_out_matrix);
+
+
+  // --- Turns off the ESP32-CAM white on-board LED (flash) connected to GPIO 4 ---
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+  rtc_gpio_hold_en(GPIO_NUM_4);
+  delay(2000);
+
+
+  // --- Deep sleep ---
+  Serial.println("Going to sleep now");
+  delay(2000);
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");
+}
+
+int raw_feature_get_data(size_t offset, size_t out_len, float *signal_ptr)
+{
+  size_t pixel_ix = offset * 3;
+  size_t bytes_left = out_len;
+  size_t out_ptr_ix = 0;
+
+  // read byte for byte
+  while (bytes_left != 0) {
+    // grab the values and convert to r/g/b
+    uint8_t r, g, b;
+    r = resized_matrix->item[pixel_ix];
+    g = resized_matrix->item[pixel_ix + 1];
+    b = resized_matrix->item[pixel_ix + 2];
+
+    // then convert to out_ptr format
+    float pixel_f = (r << 16) + (g << 8) + b;
+    signal_ptr[out_ptr_ix] = pixel_f;
+
+    // and go to the next pixel
+    out_ptr_ix++;
+    pixel_ix += 3;
+    bytes_left--;
+  }
+  return 0;
+}
+
+
+void classify()
+{
+  Serial.println("Getting signal...");
+  signal_t signal;
+  signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_WIDTH;
+  signal.get_data = &raw_feature_get_data;
+
+  Serial.println("Run classifier...");
+  // Feed signal to the classifier
+  EI_IMPULSE_ERROR res = run_classifier(&signal, &result, false /* debug */);
+
+  // Returned error variable "res" while data object.array in "result"
+  ei_printf("run_classifier returned: %d\n", res);
+  if (res != 0)
+    return;
+
+  // print the predictions
+  ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+            result.timing.dsp, result.timing.classification, result.timing.anomaly);
+  for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+    ei_printf("    %s: \t%f\r\n", result.classification[ix].label, result.classification[ix].value);
+  }
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+  ei_printf("    anomaly score: %f\r\n", result.anomaly);
+#endif
+}
+
+void loop() {
+  take_picture();
+}
